c5979dec65964afd2cfaa7812c38aa21e5370cb2
Andrej.Feller <a.feller@intabia.ru>
2019-07-05 15:02:29 +0700

    Initial commit

---
+++ src/test/java/OpenBrowserTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class OpenBrowserTest {
+
+    @Test
+    public void test(){
+        assertThat(1, is(1));
+    }
+}
