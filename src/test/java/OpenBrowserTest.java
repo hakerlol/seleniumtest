e552f51fb78e30e10d1e1f353e4698464788fb83
Andrej.Feller <a.feller@intabia.ru>
2019-07-10 15:51:35 +0700

    write small test

--- src/test/java/OpenBrowserTest.java
+++ src/test/java/OpenBrowserTest.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,7 +15,7 @@ public class OpenBrowserTest {
 
     @BeforeTest
     public void setUp(){
-        url = "https://google.com";
+        url = "https://www.google.com/";
         driver = new ChromeDriver();
     }
 
@@ -22,6 +26,14 @@ public class OpenBrowserTest {
 
     @Test
     public void test(){
+        driver.get(url);
+        WebDriverWait driverWait = new WebDriverWait(driver, 5);
+        By searchWindow = By.xpath("/html/body/div/div[3]/form/div[2]/div/div[1]/div/div[1]/input");
+        WebElement searchWindowElement = driverWait.until(ExpectedConditions.presenceOfElementLocated(searchWindow));
+        searchWindowElement.click();
+        searchWindowElement.sendKeys("How to google");
+        WebElement startSearch = driver.findElement(By.xpath("/html/body/div/div[3]/form/div[2]/div/div[3]/center/input[1]"));
+        startSearch.click();
 
     }
 }
