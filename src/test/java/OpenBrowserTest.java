91933bfd42f4ae346818314de09b4b4e857f60c6
Andrej.Feller <a.feller@intabia.ru>
2019-08-13 12:21:27 +0700

    Add smth new

--- src/test/java/OpenBrowserTest.java
+++ src/test/java/OpenBrowserTest.java
@@ -1,4 +1,10 @@
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,6 +13,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class OpenBrowserTest {
@@ -24,9 +34,27 @@ public class OpenBrowserTest {
         driver.close();
     }
 
-    @Test
-    public void test(){
+    @Step("Открытие браузера. URL адрес: {url}")
+    private void openBrowser(String url){
         driver.get(url);
+    }
+
+    @Attachment(value = "Думай!!!", fileExtension = ".jpeg")
+    private static byte[] getBytesAnnotation(String resourceName) throws IOException {
+        return Files.readAllBytes(Paths.get("src/test/resources", resourceName));
+    }
+
+    @Attachment
+    public byte[] attachScreenshot() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Test
+    @Story(value = "Открытие браузера")
+    @Description(value = "Тест проверяет открытие стартовой страницы google.com и возможность поиска")
+    public void test() throws IOException {
+        openBrowser(url);
+        getBytesAnnotation("1.jpeg");
         WebDriverWait driverWait = new WebDriverWait(driver, 5);
         By searchWindow = By.xpath("/html/body/div/div[3]/form/div[2]/div/div[1]/div/div[1]/input");
         WebElement searchWindowElement = driverWait.until(ExpectedConditions.presenceOfElementLocated(searchWindow));
@@ -34,5 +62,18 @@ public class OpenBrowserTest {
         searchWindowElement.sendKeys("How to google");
         WebElement startSearch = driver.findElement(By.xpath("/html/body/div/div[3]/form/div[2]/div/div[3]/center/input[1]"));
         startSearch.click();
+        attachScreenshot();
+        searchWindowElement.sendKeys("2323");
+    }
+
+    @Test
+    @Story(value = "Поиск картинок")
+    @Description(value = "Тест проверяет открытие страницы с картинками")
+    public void testPic() throws IOException {
+        openBrowser("https://www.google.ru/search?newwindow=1&hl=ru&tbm=isch&source=hp&biw=960&bih=900&ei=eOkmXZetKb2HwPAP4omowAY&q=s&oq=s&gs_l=img.12..0l10.4166.4166..4894...0.0..0.134.273.2j2......0....1..gws-wiz-img.....0.XT827_bPiQ8");
+        WebDriverWait driverWait = new WebDriverWait(driver, 5);
+        By searchWindow = By.id("0b3pyJvzyKZqIM:");
+        WebElement searchWindowElement = driverWait.until(ExpectedConditions.presenceOfElementLocated(searchWindow));
+        searchWindowElement.click();
     }
 }
